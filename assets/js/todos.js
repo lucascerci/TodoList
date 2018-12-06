e540f76ef789283c49dfc036503c4eba0b9f7fb3
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2018-12-06 18:47:08 -0200

    add files

---
+++ assets/js/todos.js
@@ -0,0 +1,48 @@
+//TUDO ISSO EM UMA LINHA POR CAUSA DA CRIAÇÃO DE UMA CLASSE
+
+// $("li").click(function(){
+// 	if($(this).css("color") === "rgb(128, 128, 128)"){
+// 		$(this).css({
+// 			color: "black",
+// 			textDecoration: "none"
+// 		});	
+// 	}else{
+// 		$(this).css({
+// 		color: "gray",
+// 		textDecoration: "line-through"
+// 		});	
+// 	}
+// });
+
+// POR CAUSA DA CRIAÇÃO DA CLASSE .completed
+
+// codigo simples -> 
+
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+
+//click on X to delete Todo
+
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabing new todo text from input
+		var todoText = ($(this).val());
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash-alt'></i></span>" + todoText + "</li>")
+	}
+});
+
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
